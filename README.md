bcac1062686b16d23e21295f8c88258aa6390e60
Soumo Sarkar <soumosarkar297@gmail.com>
2025-01-24 11:56:55 +0530

    File encryption and decryption working

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# file-cryptor
-File Encryption and Decryption in C++
+# File Cryptor
+A simple C++ application for encrypting and decrypting files in bulk.
+
+
+## Features
+- Encrypt or decrypt all files within a specified folder.
+- Easy-to-use command-line interface.
+- Built with modern C++ standards (C++17) for performance and reliability.
+
+
+
+## How to use:
+### 1. Clone the Repository
+```bash
+git clone https://github.com/SuperSection/file-cryptor
+cd file-cryptor
+```
+
+### 2. Build the Executables
+Use the make command to build the required binaries:
+```bash
+make all
+```
+
+### 3. Run the Application
+To encrypt or decrypt files in a folder, run:
+```bash
+./encrypt_decrypt
+```
+
+### 4. Clean the Build
+To remove all compiled files and executables:
+```bash
+make clean
+```
+
+
+
+## Requirements
+- Compiler: g++ (with C++17 support)
+- Build Tool: make
+- Debugger (optional): gdb or lldb for debugging.
+
+
+## Author
+Developed by Soumo Sarkar.
+Feel free to reach out with feedback or questions!
--- main.cpp
+++ main.cpp
@@ -11,16 +11,17 @@ int main(int argc, char const *argv[]) {
   std::string directory;
   std::string action;
 
-  std::cout << "Enter the directory path: " << std::endl;
+  std::cout << "Enter the directory path: ";
   std::getline(std::cin, directory);
 
-  std::cout << "Enter the action (encrypt/decrypt): " << std::endl;
+  std::cout << "Enter the action (encrypt/decrypt): ";
   std::getline(std::cin, action);
 
 
   try {
     if (fs::exists(directory) && fs::is_directory(directory)) {
       ProcessManagement processManagement;
+
       for (const auto &entry: fs::recursive_directory_iterator(directory)) {
         if (entry.is_regular_file()) {
           std::string filePath = entry.path().string();
@@ -29,7 +30,7 @@ int main(int argc, char const *argv[]) {
           std::fstream f_stream = std::move(io.getFileStream());
 
           if (f_stream.is_open()) {
-            Action taskAction = (action == "ENCRYPT" ? Action::ENCRYPT : Action::DECRYPT);
+            Action taskAction = (action == "ENCRYPT" || action == "encrypt" ? Action::ENCRYPT : Action::DECRYPT);
             auto task = std::make_unique<Task>(std::move(f_stream), taskAction, filePath);
             processManagement.submitToQueue(std::move(task));
           }
